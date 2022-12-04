b84bcdec12d3fd4c130094a8c32323e2a13e6fb0
Heinrich Schmidt <40140403+Heinrich-Schmidt@users.noreply.github.com>
2022-12-05 00:22:40 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    id="box"
+    @mousedown="mouseDown"
+  >
+  <MovingObject
+    v-for="(item, index) in items"
+    :key="index"
+    :position="item[positionNumber]"
+  />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MovingObject from './components/MovingObject.vue'
+
+let items = [] //массив, содержащий координаты положений для каждого элемента
+let windowSize = [document.documentElement.clientWidth, document.documentElement.clientHeight] //размеры окна
+let elW = Math.floor(windowSize[0]*0.05) //ширина элемента
+let elH = Math.floor(windowSize[0]*0.05*1.4065) //высота элемента
+let gap = Math.floor(elW / 96 ) //размер сдвига карт в колоде
+if (gap == 0) gap = 1
+let groupWidth = Math.floor((windowSize[0]-16)/5) //ширина экрана, выделенная под колоду
+let groupWMargin =Math.floor( groupWidth - ( gap*2*49 + elW)/2) //горизонтальный отступ между колодами
+let groupHeight= Math.floor((windowSize[1]-16)/2) //высота экрана, выделенная под колоду
+let groupHMargin =Math.floor( (groupHeight - ( gap*49 + elH)/2)/2) //вернтикальный отступ между колодами
+let lineWMargin = Math.floor((windowSize[0]- elW - 16)/49) //горизонтальный отступ разложенных карт
+let lineHMargin = Math.floor((windowSize[1]- elH - 16)/9) //вертикальный отступ разложенных карт
+let lineGap = Math.floor((windowSize[0] - (lineWMargin*49+elW+16))/2) //отступ для вертикального выравнивания всех разложенных карт
+
+for(let i = 0; i<500; i++){
+  let group = Math.floor(i/50)
+  let line = Math.floor(i/250)
+  items[i] = [[((i - 50*group) * gap*2 + group*groupWMargin + groupWMargin - 5*groupWMargin*line),(i - 50*group + line*groupHMargin*2 + groupHMargin)],[(i - 50*group)*lineWMargin + lineGap, group*lineHMargin]]
+}
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MovingObject,
+  },
+  data() {
+    return {
+      items: items,
+      positionNumber: 0
+    }
+  },
+  methods: {
+    mouseDown() 
+    {
+    console.log(11)
+    if(this.positionNumber == 1){
+      this.positionNumber = 0
+    }else{
+      this.positionNumber = 1
+    }
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#box {
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(0deg, rgb(153 127 115) 0%, rgb(215 215 215) 100%);
+  padding: 8px;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/MovingObject.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="MovingObject" 
+    :style="{
+        transform:`translate(${position[0]}px, ${position[1]}px)`, 
+        }"></div>
+</template>
+
+<script>
+export default {
+  name: 'MovingObject',
+  props: {
+    position: Array,
+  }
+}
+</script>
+
+<style scoped>
+.MovingObject{
+  position: absolute;
+    width: 5vw;
+    height: calc(5vw * 1.4065);
+    max-width: 256px;
+    max-height: 360px;
+    background-image: url(../assets/img.png);
+    background-size: contain;
+    display: inline-block;
+    box-shadow: 2px 2px 4px 0px #444;
+    border-radius: 0.25vw;
+    transition: 0.8s cubic-bezier(0.21, 0.29, 0.61, 0.99);
+    pointer-events: none;
+}
+</style>
